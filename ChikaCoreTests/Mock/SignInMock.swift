524ebb38dec6f6a3a3e10e9031341a61bfcb7618
Mounir Ybanez <rinuom91@gmail.com>
2018-01-09 00:42:42 +0800

    Added SignIn mock.

---
+++ ChikaCoreTests/Mock/SignInMock.swift
@@ -0,0 +1,17 @@
+//
+//  SignInMock.swift
+//  ChikaCoreTests
+//
+//  Created by Mounir Ybanez on 1/8/18.
+//  Copyright © 2018 Nir. All rights reserved.
+//
+
+import ChikaCore
+
+class SignInMock: SignIn {
+
+    func signIn(withEmail: String, password: String, completion: @escaping (Result<Auth>) -> Void) -> Bool {
+        completion(.ok(Auth()))
+        return true
+    }
+}
