f8e46f6a9e0ff64f19705055466c6055857ee7fd
Mounir Ybanez <rinuom91@gmail.com>
2018-01-09 01:02:12 +0800

    Added SignOut mock.

---
+++ ChikaCoreTests/Mock/SignOutMock.swift
@@ -0,0 +1,17 @@
+//
+//  SignOutMock.swift
+//  ChikaCoreTests
+//
+//  Created by Mounir Ybanez on 1/9/18.
+//  Copyright © 2018 Nir. All rights reserved.
+//
+
+import ChikaCore
+
+class SignOutMock: SignOut {
+
+    func signOut(withCompletion completion: @escaping (Result<OK>) -> Void) -> Bool {
+        completion(.ok(OK("")))
+        return true
+    }
+}
