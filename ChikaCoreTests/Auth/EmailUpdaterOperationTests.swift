cd7ac7d201b549b65d8b6b48eb374bbaef1af5df
Mounir Ybanez <rinuom91@gmail.com>
2018-01-08 14:39:50 +0800

    Added tests on EmailUpdaterOperation.

---
+++ ChikaCoreTests/Auth/EmailUpdaterOperationTests.swift
@@ -0,0 +1,53 @@
+//
+//  EmailUpdaterOperationTests.swift
+//  ChikaCoreTests
+//
+//  Created by Mounir Ybanez on 1/8/18.
+//  Copyright © 2018 Nir. All rights reserved.
+//
+
+import XCTest
+@testable import ChikaCore
+
+class EmailUpdaterOperationTests: XCTestCase {
+    
+    func testUpdateEmailA() {
+        let updater = EmailUpdaterMock()
+        let operation = EmailUpdaterOperation()
+        var ok = operation.updateEmail(using: updater)
+        XCTAssertFalse(ok)
+        
+        ok = operation.withNewEmail("me@me.com").updateEmail(using: updater)
+        XCTAssertFalse(ok)
+        
+        ok = operation.withCurrentEmail("me@me.com").updateEmail(using: updater)
+        XCTAssertFalse(ok)
+        
+        ok = operation.withCurrentPassword("me12345").updateEmail(using: updater)
+        XCTAssertFalse(ok)
+        
+        ok = operation.withNewEmail("me12345").withCurrentEmail("me@me.com").updateEmail(using: updater)
+        XCTAssertFalse(ok)
+        
+        ok = operation.withNewEmail("me12345").withCurrentPassword("me12345").updateEmail(using: updater)
+        XCTAssertFalse(ok)
+        
+        ok = operation.withCurrentEmail("me12345").withCurrentPassword("me12345").updateEmail(using: updater)
+        XCTAssertFalse(ok)
+        
+        ok = operation.withNewEmail("you@you.com").withCurrentEmail("me@me.com").withCurrentPassword("me12345").withCompletion({ _ in
+        }).updateEmail(using: updater)
+    }
+    
+    func testUpdateEmailB() {
+        let exp = expectation(description: "testUpdateEmailB")
+        let updater = EmailUpdaterMock()
+        let operation = EmailUpdaterOperation()
+        let completion: (Result<OK>) -> Void = { _ in
+            exp.fulfill()
+        }
+        let ok = operation.withNewEmail("you@you.com").withCurrentEmail("me@me.com").withCurrentPassword("me12345").withCompletion(completion).updateEmail(using: updater)
+        XCTAssertTrue(ok)
+        wait(for: [exp], timeout: 1.0)
+    }
+}
