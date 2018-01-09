f07a886ef0b67d5f7da354218308cb2d58cc7a52
Mounir Ybanez <rinuom91@gmail.com>
2018-01-09 14:19:24 +0800

    Added ChatCreatorOperation tests.

---
+++ ChikaCoreTests/Writer/ChatCreatorOperationTests.swift
@@ -0,0 +1,56 @@
+//
+//  ChatCreatorOperationTests.swift
+//  ChikaCoreTests
+//
+//  Created by Mounir Ybanez on 1/9/18.
+//  Copyright © 2018 Nir. All rights reserved.
+//
+
+import XCTest
+@testable import ChikaCore
+
+class ChatCreatorOperationTests: XCTestCase {
+    
+    func testCreateChatA() {
+        let creator = ChatCreatorMock()
+        let operation = ChatCreatorOperation()
+        
+        var ok = operation.createChat(using: creator)
+        XCTAssertFalse(ok)
+        
+        ok = operation.withTitle("chat:title").createChat(using: creator)
+        XCTAssertFalse(ok)
+        
+        ok = operation.withMessage("chat:message").createChat(using: creator)
+        XCTAssertFalse(ok)
+        
+        ok = operation.withParticipantIDs([]).createChat(using: creator)
+        XCTAssertFalse(ok)
+        
+        ok = operation.withTitle("chat:title").withMessage("chat:message").createChat(using: creator)
+        XCTAssertFalse(ok)
+        
+        ok = operation.withTitle("chat:title").withParticipantIDs([]).createChat(using: creator)
+        XCTAssertFalse(ok)
+        
+        ok = operation.withMessage("chat:message").withParticipantIDs([]).createChat(using: creator)
+        XCTAssertFalse(ok)
+        
+        ok = operation.withTitle("chat:title").withMessage("chat:message").withParticipantIDs([]).createChat(using: creator)
+        XCTAssertTrue(ok)
+    }
+    
+    func testCreateChatB() {
+        let exp = expectation(description: "testCreateChatB")
+        let creator = ChatCreatorMock()
+        let operation = ChatCreatorOperation()
+        let completion: (Result<Chat>) -> Void = { _ in
+            exp.fulfill()
+        }
+        
+        let ok = operation.withTitle("chat:title").withMessage("chat:message").withParticipantIDs([]).withCompletion(completion).createChat(using: creator)
+        XCTAssertTrue(ok)
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+}
