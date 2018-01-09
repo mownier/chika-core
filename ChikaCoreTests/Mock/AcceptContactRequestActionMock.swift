3f3ec8cb5566a2f2082bccf7cc1e9c5aa50f8016
Mounir Ybanez <rinuom91@gmail.com>
2018-01-09 22:38:07 +0800

    Added AcceptContactRequestAction mock.

---
+++ ChikaCoreTests/Mock/AcceptContactRequestActionMock.swift
@@ -0,0 +1,17 @@
+//
+//  AcceptContactRequestActionMock.swift
+//  ChikaCoreTests
+//
+//  Created by Mounir Ybanez on 1/9/18.
+//  Copyright © 2018 Nir. All rights reserved.
+//
+
+import ChikaCore
+
+class AcceptContactRequestActionMock: AcceptContactRequestAction {
+
+    func acceptContactRequest(withID id: ID, completion: @escaping (Result<OK>) -> Void) -> Bool {
+        completion(.ok(OK("")))
+        return true
+    }
+}
