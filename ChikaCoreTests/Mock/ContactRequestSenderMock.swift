d3126e5ffe1eb360796be636eb011bad43cf6e71
Mounir Ybanez <rinuom91@gmail.com>
2018-01-09 22:58:12 +0800

    Added ContactRequestSender mock.

---
+++ ChikaCoreTests/Mock/ContactRequestSenderMock.swift
@@ -0,0 +1,17 @@
+//
+//  ContactRequestSenderMock.swift
+//  ChikaCoreTests
+//
+//  Created by Mounir Ybanez on 1/9/18.
+//  Copyright © 2018 Nir. All rights reserved.
+//
+
+import ChikaCore
+
+class ContactRequestSenderMock: ContactRequestSender {
+
+    func sendContactRequest(to personID: ID, completion: @escaping (Result<OK>) -> Void) -> Bool {
+        completion(.ok(OK("")))
+        return true
+    }
+}
