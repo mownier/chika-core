cb36cc35c659f813fe21a7468c3d9626dd7e57b1
Mounir Ybanez <rinuom91@gmail.com>
2018-01-09 23:13:55 +0800

    Added MessageCreator mock.

---
+++ ChikaCoreTests/Mock/MessageCreatorMock.swift
@@ -0,0 +1,17 @@
+//
+//  MessageCreatorMock.swift
+//  ChikaCoreTests
+//
+//  Created by Mounir Ybanez on 1/9/18.
+//  Copyright © 2018 Nir. All rights reserved.
+//
+
+import ChikaCore
+
+class MessageCreatorMock: MessageCreator {
+
+    func createMessage(for chatID: ID, participantIDs: [ID], content: String, completion: @escaping (Result<Message>) -> Void) -> Bool {
+        completion(.ok(Message()))
+        return true
+    }
+}
