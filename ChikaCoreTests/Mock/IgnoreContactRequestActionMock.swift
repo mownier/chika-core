050291ce4183a9e5d45b2d1f06fb3e7048e90f14
Mounir Ybanez <rinuom91@gmail.com>
2018-01-09 23:03:35 +0800

    Added IgnoreContactRequestAction mock.

---
+++ ChikaCoreTests/Mock/IgnoreContactRequestActionMock.swift
@@ -0,0 +1,17 @@
+//
+//  IgnoreContactRequestActionMock.swift
+//  ChikaCoreTests
+//
+//  Created by Mounir Ybanez on 1/9/18.
+//  Copyright © 2018 Nir. All rights reserved.
+//
+
+import ChikaCore
+
+class IgnoreContactRequestActionMock: IgnoreContactRequestAction {
+
+    func ignoreContactRequest(withID id: ID, completion: @escaping (Result<OK>) -> Void) -> Bool {
+        completion(.ok(OK("")))
+        return true
+    }
+}
