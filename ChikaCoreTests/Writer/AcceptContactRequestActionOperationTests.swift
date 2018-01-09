e39576f4adfb98ee6bc090aa03469c1aef5e6432
Mounir Ybanez <rinuom91@gmail.com>
2018-01-09 22:38:36 +0800

    Added AcceptContactRequestActionOperation tests.

---
+++ ChikaCoreTests/Writer/AcceptContactRequestActionOperationTests.swift
@@ -0,0 +1,40 @@
+//
+//  AcceptContactRequestActionOperationTests.swift
+//  ChikaCoreTests
+//
+//  Created by Mounir Ybanez on 1/9/18.
+//  Copyright © 2018 Nir. All rights reserved.
+//
+
+import XCTest
+@testable import ChikaCore
+
+class AcceptContactRequestActionOperationTests: XCTestCase {
+    
+    func testAcceptContactRequestA() {
+        let action = AcceptContactRequestActionMock()
+        let operation = AcceptContactRequestActionOperation()
+        let id = ID("contact:request:1")
+        
+        var ok = operation.acceptContactRequest(using: action)
+        XCTAssertFalse(ok)
+        
+        ok = operation.withContactRequestID(id).acceptContactRequest(using: action)
+        XCTAssertTrue(ok)
+    }
+    
+    func testAcceptContactRequestB() {
+        let id = ID("contact:request:1")
+        let exp = expectation(description: "testAcceptContactRequestB")
+        let action = AcceptContactRequestActionMock()
+        let operation = AcceptContactRequestActionOperation()
+        let completion: (Result<OK>) -> Void = { _ in
+            exp.fulfill()
+        }
+        
+        let ok = operation.withContactRequestID(id).withCompletion(completion).acceptContactRequest(using: action)
+        XCTAssertTrue(ok)
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+}
