44a89d6bb29d963d9d377469865a5b3bf3723603
Mounir Ybanez <rinuom91@gmail.com>
2018-01-09 22:58:30 +0800

    Added ContactRequestSenderOperation tests.

---
+++ ChikaCoreTests/Writer/ContactRequestSenderOperationTests.swift
@@ -0,0 +1,39 @@
+
+//
+//  ContactRequestSenderOperationTests.swift
+//  ChikaCoreTests
+//
+//  Created by Mounir Ybanez on 1/9/18.
+//  Copyright © 2018 Nir. All rights reserved.
+//
+
+import XCTest
+@testable import ChikaCore
+
+class ContactRequestSenderOperationTests: XCTestCase {
+    
+    func testSendContactRequestA() {
+        let sender = ContactRequestSenderMock()
+        let operation = ContactRequestSenderOperation()
+        
+        var ok = operation.sendContactRequest(using: sender)
+        XCTAssertFalse(ok)
+        
+        ok = operation.withPersonID(ID("person:1")).sendContactRequest(using: sender)
+        XCTAssertTrue(ok)
+    }
+    
+    func testSendContactRequestB() {
+        let exp = expectation(description: "testSendContactRequestB")
+        let sender = ContactRequestSenderMock()
+        let operation = ContactRequestSenderOperation()
+        let completion: (Result<OK>) -> Void = { _ in
+            exp.fulfill()
+        }
+        
+        let ok = operation.withPersonID(ID("person:1")).withCompletion(completion).sendContactRequest(using: sender)
+        XCTAssertTrue(ok)
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+}
