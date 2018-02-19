2bb4dd09e00ee22eba047f7af97647f14fb0b399
Mounir Ybanez <rinuom91@gmail.com>
2018-02-19 22:39:12 +0800

    Added photoURL and createdOn variables.

--- ChikaCore/Source/Model/Store/Chat.swift
+++ ChikaCore/Source/Model/Store/Chat.swift
@@ -11,13 +11,17 @@ public struct Chat: Hashable, Unique {
     public var id: ID
     public var title: String
     public var recent: Message
+    public var photoURL: String
     public var creatorID: ID
+    public var createdOn: Date
     
     public init() {
         self.id = ID("")
         self.title = ""
         self.recent = Message()
+        self.photoURL = ""
         self.creatorID = ID("")
+        self.createdOn = Date()
     }
     
     public var hashValue: Int {
